f4afd89cfb3543f7a1a38d9efba30b2f4b4f47eb
Neha N Murthy <57165708+nehamurthy2661@users.noreply.github.com>
2021-09-03 22:32:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+
+document.addEventListener("keypress",function(event){
+
+    drumPlay(event.key.toLowerCase());
+    })
+buttonList=document.querySelectorAll("button")
+for(var i=0;i<7;i++){
+buttonList[i].addEventListener("click",function(){
+drumPlay(this.textContent);
+})
+}
+function drumPlay(letter){
+    document.querySelector("."+letter).classList.add("pressed");
+    playAudio(letter);
+    setTimeout(()=>{ document.querySelector("."+letter).classList.remove("pressed");},100)
+ }
+
+function playAudio(letter){
+    switch(letter){
+        
+        case 'w':
+            var audio=new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case 'a':
+            var audio=new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case 's':        
+            var audio=new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case 'd':
+            var audio=new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case 'j':
+            var audio=new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case 'k':
+            var audio=new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case 'l':
+            var audio=new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+    }
+}
